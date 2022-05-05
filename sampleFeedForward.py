1e7d11052066c5421400a48fade10846a0fcf3a0
Sheetal Rani Prasad <sprasad9091@sdsu.edu>
2022-05-05 15:19:24 -0700

    retry

--- sampleFeedForward.py
+++ sampleFeedForward.py
@@ -1,96 +1,116 @@
-from random import shuffle
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 #device config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 #hyper parameter
-input_size = 784
+input_size = 14
 hidden_size = 100
 num_classes = 10
 num_epochs = 2
 batch_size = 100
 learning_rate = 0.001
 
-train_dataset = torchvision.datasets.MNIST(root='./data', train = True, transform=transforms.ToTensor(), download = True)
+finance_dataframe = pd.read_csv('data/targetfirm_prediction_dataset_small.csv')
 
-test_dataset = torchvision.datasets.MNIST(root = './data', train = False, transform = transforms.ToTensor())
+finance_dataframe.fillna(0,inplace=True)
+
+finance_features = finance_dataframe.iloc[:,4:len(finance_dataframe.columns)]
+y = finance_dataframe['target']
+
+X_train, X_test, y_train, y_test = train_test_split(finance_features, y, test_size=0.3,random_state=100)
+X_train.reset_index(inplace=True)
+train_dataset = X_train.iloc[:10,:]
+test_dataset = X_test.iloc[:10,:]
+
+print(train_dataset)
+
+# train_dataset = torchvision.datasets.MNIST(root='./data', train = True, transform=transforms.ToTensor(), download = True)
+
+# test_dataset = torchvision.datasets.MNIST(root = './data', train = False, transform = transforms.ToTensor())
 
 train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
 
 test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
 
+
 examples = iter(train_loader)
-samples, labels = examples.next()
-print(samples.shape, labels.shape)
 
-for i in range(6):
-    plt.subplot(2, 3, i+1)
-    plt.imshow(samples[i][0], cmap='gray')
+print(examples.next())
 
-plt.show()
+# for d in examples:
+#     print(d)
+
+# for i in range(6):
+#     plt.subplot(2, 3, i+1)
+#     plt.imshow(samples[i][0], cmap='gray')
+
+# plt.show()
 
 
-# Fully connected neural network with one hidden layer
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.input_size = input_size
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.relu = nn.ReLU()
-        self.l2 = nn.Linear(hidden_size, num_classes)  
+# # Fully connected neural network with one hidden layer
+# class NeuralNet(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_classes):
+#         super(NeuralNet, self).__init__()
+#         self.input_size = input_size
+#         self.l1 = nn.Linear(input_size, hidden_size) 
+#         self.relu = nn.ReLU()
+#         self.l2 = nn.Linear(hidden_size, num_classes)  
     
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        # no activation and no softmax at the end
-        return out
+#     def forward(self, x):
+#         out = self.l1(x)
+#         out = self.relu(out)
+#         out = self.l2(out)
+#         # no activation and no softmax at the end
+#         return out
 
-model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+# model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+# # Loss and optimizer
+# criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
-# Train the model
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
-        # origin shape: [100, 1, 28, 28]
-        # resized: [100, 784]
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
+# # Train the model
+# n_total_steps = len(train_loader)
+# for epoch in range(num_epochs):
+#     for i, (images, labels) in enumerate(train_loader):  
+#         # origin shape: [100, 1, 28, 28]
+#         # resized: [100, 784]
+#         images = images.reshape(-1, 1*14).to(device)
+#         labels = labels.to(device)
         
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+#         # Forward pass
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
         
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
         
-        if (i+1) % 100 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+#         if (i+1) % 100 == 0:
+#             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
-# Test the model
-# In test phase, we don't need to compute gradients (for memory efficiency)
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    for images, labels in test_loader:
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs.data, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
+# # Test the model
+# # In test phase, we don't need to compute gradients (for memory efficiency)
+# with torch.no_grad():
+#     n_correct = 0
+#     n_samples = 0
+#     for images, labels in test_loader:
+#         images = images.reshape(-1, 28*28).to(device)
+#         labels = labels.to(device)
+#         outputs = model(images)
+#         # max returns (value ,index)
+#         _, predicted = torch.max(outputs.data, 1)
+#         n_samples += labels.size(0)
+#         n_correct += (predicted == labels).sum().item()
 
-    acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network on the 10000 test images: {acc} %')
+#     acc = 100.0 * n_correct / n_samples
+#     print(f'Accuracy of the network on the 10000 test images: {acc} %')
